21cfb668d1c46cf9a7b69617942e42fb75e26bb5
Carson Tang <tang.carson@gmail.com>
2015-05-28 07:54:02 -0700

    remove config files if they exist

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+if [ -f ~/.bashrc ]; then rm ~/.bashrc; fi
+if [ -f ~/.bash_profile ]; then rm ~/.bash_profile; fi
+if [ -f ~/.gitconfig ]; then rm ~/.gitconfig; fi
+if [ -f ~/.tmux.conf ]; then rm ~/.tmux.conf; fi
+if [ -f ~/.vimrc ]; then rm ~/.vimrc; fi
+
 ln -s ~/code/dotfiles/bashrc ~/.bashrc
 ln -s ~/code/dotfiles/profile ~/.bash_profile
 ln -s ~/code/dotfiles/gitconfig ~/.gitconfig
