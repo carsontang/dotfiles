7607e59178a5bd3b65c3083949cc95c8f866f314
Carson Tang <tang.carson@gmail.com>
2017-03-10 00:35:01 -0800

    Introduced LS_COLOR

--- bashrc
+++ bashrc
@@ -1,17 +1,17 @@
 # Command line, Git aliases
-source ~/code/dotfiles/aliases
-source ~/code/dotfiles/env
-source ~/code/dotfiles/functions
+source ~/dotfiles/aliases
+source ~/dotfiles/env
+source ~/dotfiles/functions
 
 # Bash coloring
 function bash_prompt
 {
   local RED="\[\e[1;31m\]"
-  local BLUE="\[\e[0;36m\]"
+  local CYAN="\[\e[0;36m\]"
   local BLACK="\[\e[1;30m\]"
   local GREEN="\[\033[0;32m\]"
   local NO_COLOR="\[\033[0m\]"
-  PS1="${BLACK}[${GREEN}\u${BLUE}:${BLACK}\w${BLACK}]${BLUE}$ "
+  PS1="${CYAN}[${GREEN}\u${CYAN}:${CYAN}\w${CYAN}]${NO_COLOR}$ "
 }
 
 bash_prompt
--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
 #! /bin/bash
 
-if [ -f ~/.bashrc ]; then rm ~/.bashrc; fi
-if [ -f ~/.bash_profile ]; then rm ~/.bash_profile; fi
-if [ -f ~/.gitconfig ]; then rm ~/.gitconfig; fi
-if [ -f ~/.tmux.conf ]; then rm ~/.tmux.conf; fi
-if [ -f ~/.vimrc ]; then rm ~/.vimrc; fi
+rm ~/.bashrc;
+rm ~/.bash_profile;
+rm ~/.gitconfig;
+rm ~/.tmux.conf;
+rm ~/.vimrc;
 
-ln -s ~/code/dotfiles/bashrc ~/.bashrc
-ln -s ~/code/dotfiles/profile ~/.bash_profile
-ln -s ~/code/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/code/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/code/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/bashrc ~/.bashrc
+ln -s ~/dotfiles/profile ~/.bash_profile
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/vimrc ~/.vimrc
+
+if [ ! -e ~/.vim/autoload/pathogen.vim ]; then
+	mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+else
+	echo "vim pathogen is installed."
+fi
